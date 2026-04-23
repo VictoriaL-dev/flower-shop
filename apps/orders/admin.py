8c71964114e199b747f74670c0dad1970ef8b321
Victoria <logunova280702@gmail.com>
2026-04-23 14:12:35 +0700

    Create order model and set up the admin panel

--- apps/orders/admin.py
+++ apps/orders/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Order
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", "phone", "status", "delivery_time"]
+    list_display_links = ["id", "name"]
+    list_filter = ["status", "delivery_time", "created_at"]
+    search_fields = ["name", "phone", "address", "payment_id"]
+    list_editable = ["status"]
+    readonly_fields = ["created_at", "payment_id"]
+    list_per_page = 20
+
+    fieldsets = (
+        ("Данные клиента", {
+            "fields": ["name", "phone", "address"]
+        }),
+        ("Детали заказа", {
+            "fields": ["status", "delivery_time", "bouquet", "price", "comment"]
+        }),
+        ("Служебная информация", {
+            "fields": ["created_at", "payment_id"],
+            "classes": ["collapse"]
+        })
+    )
--- flower_shop/settings.py
+++ flower_shop/settings.py
@@ -21,6 +21,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "phonenumber_field",
     "apps.pages.apps.PagesConfig",
     "apps.bouquets.apps.BouquetsConfig",
     "apps.orders.apps.OrdersConfig",
