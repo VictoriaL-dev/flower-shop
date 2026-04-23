4042b06975a007bc12507b923af5e4d52edcd734
Victoria <logunova280702@gmail.com>
2026-04-23 16:10:08 +0700

    Add order link to card.html

--- apps/bouquets/views.py
+++ apps/bouquets/views.py
@@ -8,6 +8,6 @@ def catalog(request):
     return render(request, "bouquets/catalog.html", context)
 
 
-def card(request, pk):
-    bouquet = get_object_or_404(Bouquet, pk=pk)
+def card(request, bouquet_id):
+    bouquet = get_object_or_404(Bouquet, pk=bouquet_id)
     return render(request, "bouquets/card.html", {"bouquet": bouquet})
